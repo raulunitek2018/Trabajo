cd12c1b02d4e094ee4d24ff05236096a204ad659
raulunitek2018 <rdelatorre269@gmail.com>
2017-10-17 00:35:20 -0500

    Trabajo

---
+++ MiEdad/app/src/main/java/pe/edu/unitek/miedad/MainActivity.java
@@ -0,0 +1,28 @@
+package pe.edu.unitek.miedad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etDOB;
+    TextView tvShowAge;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etDOB = (EditText) findViewById(R.id.etDOB);
+        tvShowAge = (TextView) findViewById(R.id.tvShowAge);
+    }
+    public void buFindAge(View view) {
+        int DOB = Integer.parseInt(etDOB.getText().toString());
+        Calendar calendar = Calendar.getInstance();
+        int year  = calendar.get(Calendar.YEAR);
+        int Age = year - DOB;
+        tvShowAge.setText("Your age is: "+Age+" Months: " + Age*12 + " Days " + Age*356);
+    }
+}
